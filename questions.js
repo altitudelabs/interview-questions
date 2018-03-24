4f370bf789ca28fbf25c273a4d07d042b4cbc291
LEE Ho Ting <21713884+vivianhtlee@users.noreply.github.com>
2018-03-24 13:28:13 +0800

    LEE Ho Ting

--- questions.js
+++ questions.js
@@ -5,10 +5,29 @@
  * Check the comment to see how the output should look!
  */
 function reverseString(str) {
-
+  
+  //for (i =0;i<str.length/2;i++){
+  //  a=str[i];
+  //  console.log(str[i]);
+  //  str[i]=str[str.length-i-1];
+  //  console.log(str[str.length-i-1]);
+  //  console.log(str[i]);
+  //  str[str.length-i-1]=a;
+  //}
+  
+  //console.log(str);
+  
+  str2="";
+  for ( i =0;i<str.length;i++){
+    str2=str2+str[str.length-i-1];
+  }
+  //console.log(str2);
+  
+  //console.log(str);
+  return str2;
 }
 
-// reverseString('hello i am declan')
+//reverseString('hello i am declan')
 // nalced ma i olleh
 
 /**
@@ -18,7 +37,25 @@ function reverseString(str) {
  * Check the comment to see how the output should look!
  */
 function reverseWords(sentence) {
+  var spaceIndex=[]; 
+  spaceIndex[0]=-1;
+  for (i=0;i<sentence.length;i++){
+    if(sentence[i]==" ")
+          spaceIndex[spaceIndex.length]=i;
+  }
+  spaceIndex[spaceIndex.length]=sentence.length;
 
+  resultStr="";
+  for (i=spaceIndex.length-2;i>=0;i--){
+    currentWord="";
+    for(j=spaceIndex[i]+1;j<spaceIndex[i+1];j++){
+      currentWord=currentWord+[sentence[j]];
+    }
+    resultStr=resultStr+currentWord;
+    if(i!=0)
+      resultStr=resultStr+' ';
+  }
+  return resultStr;
 }
 
 // reverseWords('hello i am declan')
@@ -34,10 +71,28 @@ function reverseWords(sentence) {
  * Check the comment to see how the output should look!
  */
 function reverseWordsInPlace(sentence) {
+  var spaceIndex=[]; 
+  spaceIndex[0]=-1;
+  for (i=0;i<sentence.length;i++){
+    if(sentence[i]==" ")
+          spaceIndex[spaceIndex.length]=i;
+  }
+  spaceIndex[spaceIndex.length]=sentence.length;
 
+  resultStr="";
+  for (i=0;i<spaceIndex.length-1;i++){
+    currentWord="";
+    for(j=spaceIndex[i+1];j>spaceIndex[i];j--){
+      currentWord=currentWord+[sentence[j]];
+    }
+    resultStr=resultStr+currentWord;
+    if(i!=0)
+      resultStr=resultStr+' ';
+  }
+  return resultStr;
 }
 
-// reverseWordsInPlace('hello i am declan')
+ //reverseWordsInPlace('hello i am declan')
 // olleh i ma nalced
 
 
@@ -50,10 +105,20 @@ function reverseWordsInPlace(sentence) {
 const array = [2, 2, 4, 1, 6, 5, 3, 2, 8, 8, 0, 1, 7]
 
 function uniqueArray(array) {
-
+  // sort
+  array.sort()
+  console.log(array);
+  for(i=array.length-1;i>0;i--){
+    if(array[i]==array[i-1]){
+      delete array[i];
+      //i--;
+    }
+  }
+  
+  return array;
 }
 
-// uniqueArray(array)
+ uniqueArray(array)
 // [ 2, 4, 1, 6, 5, 3, 8, 0, 7 ]
 
 
@@ -63,7 +128,11 @@ function uniqueArray(array) {
 * backwards and forwards.
 */
 function isPalindrome(str) {
-
+  for(i=0;i<str.length/2;i++){
+    if(str[i]!=str[str.length-i-1])
+      return false;
+  }
+  return true;
 }
 
 // 3a: Make the following return `true`
